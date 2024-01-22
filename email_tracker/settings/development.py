b0ba30bf827ac62b7b301c0a599e60daf25c01b1
shukurullo0516 <shukurullo0516@gmail.com>
2024-01-22 21:04:22 +0500

    Initializing project

---
+++ email_tracker/settings/development.py
@@ -0,0 +1,22 @@
+import os
+from email_tracker.settings.base import *
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "pass"),
+        "HOST": os.environ.get("DB_HOST", "tezdb"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    }
+}
