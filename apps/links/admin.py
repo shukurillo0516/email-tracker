f70b5b2714e467938345c0f9350582ee8d97ca24
shukurullo0516 <shukurullo0516@gmail.com>
2024-01-23 20:50:25 +0500

    created_at and updated_at fields added to dashboard

--- apps/links/admin.py
+++ apps/links/admin.py
@@ -6,6 +6,7 @@ from .models import *
 class LinkAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "user", "url"]
     list_display_links = ["id", "name", "user"]
+    readonly_fields = ["created_at", "updated_at"]
     raw_id_fields = ["user"]
     readonly_fields = ["url"]
 
@@ -14,3 +15,4 @@ class LinkAdmin(admin.ModelAdmin):
 class LinkRecordAdmin(admin.ModelAdmin):
     list_display = ["id", "link", "created_at"]
     list_display_links = ["id", "link", "created_at"]
+    readonly_fields = ["created_at", "updated_at"]
